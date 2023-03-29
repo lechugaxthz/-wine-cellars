39af6954b7ab65719c7e0e66ccf014ac16971bd7
lechugaxthz <lautarogonzalogarcia@outlook.com>
2023-03-29 17:54:47 -0300

    start with models and passport services

---
+++ models/wine-cellar.model.ts
@@ -0,0 +1,6 @@
+import { Model, Column, Table, Default, AllowNull } from 'sequelize-typescript'
+
+@Table
+export class WineCellar extends Model<WineCellar> {
+
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+console.log(process.env);
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.HOST,
+      port: Number(process.env.HOST_PORT),
+      username: process.env.USER,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      models: [],
+      autoLoadModels: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
